743b05e453dd37e733c9f499b7dad4efdb8cb42a
unknown <shoryachhabra308@gmail.com>
2026-08-03 14:27:15 +0530

    notifications setup11

--- frontend/src/OneSignalInit.js
+++ frontend/src/OneSignalInit.js
@@ -1,7 +1,8 @@
 import OneSignal from "react-onesignal";
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+console.log(API_BASE_URL);
 
 let initialized = false;
 
