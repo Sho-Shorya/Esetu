2e6bfd235798ce1f40f05d194402658032b87bc0
unknown <shoryachhabra308@gmail.com>
2026-08-03 14:06:14 +0530

    notifications setup9

--- backend/services/oneSignalService.js
+++ backend/services/oneSignalService.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const sendNotification = async ({ subscriptionId, title, message }) => {
   try {
+    console.log("APP ID:", process.env.ONESIGNAL_APP_ID);
+    console.log("REST KEY:", process.env.ONESIGNAL_REST_API_KEY);
     await axios.post(
       "https://api.onesignal.com/notifications",
       {
